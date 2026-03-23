9d708e6022f045b18015954b734bcd042a06dd09
geo-gkez <georgegezeris@gmail.com>
2026-03-24 01:19:11 +0200

    task4: get balance

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.34;
 contract Bank {
     mapping(address => uint256) private accountBalances;
 
+    function getBalance() external view returns (uint256) {
+        return accountBalances[msg.sender];
+    }
+
     function depositFunds() public payable {
         require(msg.value > 0, "Deposit amount must be greater than zero.");
         accountBalances[msg.sender] += msg.value;
