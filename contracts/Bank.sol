95165c3fdd6fc89c7b28214e4d7e9d598c9adb08
geo-gkez <georgegezeris@gmail.com>
2026-03-24 01:19:11 +0200

    task2: withdraw funds

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -8,4 +8,17 @@ contract Bank {
         require(msg.value > 0, "Deposit amount must be greater than zero.");
         accountBalances[msg.sender] += msg.value;
     }
+
+    function withdrawFunds(uint256 amount) public {
+        require(amount > 0, "Withdrawal amount must be greater than zero.");
+        require(
+            accountBalances[msg.sender] >= amount,
+            "Withdrawal amount must be less or equal than balance"
+        );
+
+        accountBalances[msg.sender] -= amount;
+
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        require(success, "Transfer failed");
+    }
 }
